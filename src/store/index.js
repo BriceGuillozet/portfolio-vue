4b38a151a342bb69e5553cca32e33f94bc752b06
Brice Guillozet <brice.guillozet@gmail.com>
2021-03-18 11:41:26 +0100

    finish routing and general store, beautify to go

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,35 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        projectsList: [
+            {
+                name: "Pokédex",
+                image: {
+                    source: "/img/projects/pokedex.png",
+                    alt: "Une version web de pokédex",
+                },
+                description: "Pokédex avec HTML/CSS/Node.Js/PostGreSQL"
+            },
+            {
+                name: "Acey Deucey",
+                image: {
+                    source: "/img/projects/acey deucey.png",
+                    alt: "Une version web d'un jeu de carte",
+                },
+                description: "Jeu de Carte avec HTML/CSS/JavaScript"
+            },
+            {
+                name: "O'Blog",
+                image: {
+                    source: "/img/projects/blog.png",
+                    alt: "Un blog dynamique basé sur une base de données",
+                },
+                description: "Blog avec HTML/CSS/Node.Js/PostGreSQL"
+            }
+        ]
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
 })
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,43 +2,37 @@
     <div id="projects">
         <h1>Projets</h1>
         <div id="container__project">
-            <div class="project project1">
-                <img
-                    src="img/projects/pokedex.png"
-                    alt="Une version web d'un pokédex"
-                />
-                <div class="project__name">Pokédex</div>
-                <div class="project__description">
-                    Pokédex avec HTML/CSS/Node.Js/PostGreSQL
-                </div>
-            </div>
-            <div class="project project2">
-                <img
-                    src="img/projects/acey deucey.png"
-                    alt="Une version web d'un jeu de carte"
-                />
-                <div class="project__name">Acey Deucey</div>
-                <div class="project__description">
-                    Jeu de Carte avec HTML/CSS/JavaScript
-                </div>
-            </div>
-            <div class="project project3">
-                <img
-                    src="img/projects/blog.png"
-                    alt="Un blog dynamique basé sur une base de données"
-                />
-                <div class="project__name">O'Blog</div>
-                <div class="project__description">
-                    Blog avec HTML/CSS/Node.Js/PostGreSQL
-                </div>
-            </div>
+            <ProjectsItem
+                v-for="item in projectsList"
+                :name="item.name"
+                :image="item.image"
+                :description="item.description"
+                :key="item.name"
+            />
         </div>
     </div>
 </template>
 
+<script>
+import ProjectsItem from "../components/ProjectsItem";
+import { mapState } from "vuex";
+
+export default {
+    name: "Projects",
+    components: {
+        ProjectsItem,
+    },
+    computed: {
+        ...mapState({
+            projectsList: "projectsList"
+        })
+    }
+};
+</script>
+
 <style scoped lang="scss">
 h1 {
-    font-size: 2em;
+    font-size: 3rem;
     font-weight: bold;
 }
 
@@ -59,32 +53,5 @@ h1 {
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-evenly;
-}
-
-.project {
-    width: 20%;
-    min-height: 20em;
-    border: #f1f1f1 solid 0.1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-}
-
-.project img {
-    padding: 1rem;
-    width: 100%;
-    box-shadow: 0 0.2rem 0.2rem #222733;
-}
-
-.project__name {
-    padding: 1rem;
-    font-weight: bold;
-}
-
-.project__description {
-    text-align: center;
-    font-style: italic;
-    padding-bottom: 1rem;
 }
 </style>
